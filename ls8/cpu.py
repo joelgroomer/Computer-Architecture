79d4315c2920167dc9a1a538619495531384d7e3
Joel <joelgroomer@gmail.com>
2020-09-30 21:02:47 -0700

    Step 3: Implement CPU's run() methond and Step 4: Implemnt HLT instruction handler

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,15 +2,56 @@
 
 import sys
 
+# instruction psuedonyms
+instr = {
+    0b10100000: "ADD",
+    0b10101000: "AND",
+    0b01010000: "CALL",
+    0b10100111: "CMP",
+    0b01100110: "DEC",
+    0b10100011: "DIV",
+    0b00000001: "HLT",
+    0b01100101: "INC",
+    0b01010010: "INT",
+    0b00010011: "IRET",
+    0b01010101: "JEQ",
+    0b01011010: "JGE",
+    0b01010111: "JGT",
+    0b01011001: "JLE",
+    0b01011000: "JLT",
+    0b01010100: "JMP",
+    0b01010110: "JNE",
+    0b10000011: "LD",
+    0b10000010: "LDI",
+    0b10100100: "MOD",
+    0b10100010: "MUL",
+    0b00000000: "NOP",
+    0b01101001: "NOT",
+    0b10101010: "OR",
+    0b01000110: "POP",
+    0b01001000: "PRA",
+    0b01000111: "PRN",
+    0b01000101: "PUSH",
+    0b00010001: "RET",
+    0b10101100: "SHL",
+    0b10101101: "SHR",
+    0b10000100: "ST",
+    0b10100001: "SUB",
+    0b10101011: "XOR"
+}
+
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256
-        self.register = [0] * 8
+        self.reg = [0] * 8
+        # Register 7 is the stack pointer, 0xF4 means stack is empty
+        self.reg[7] = 0xF4
         self.pc = 0
+        self.fl = 0
+        self.ram = [0] * 256
 
     def ram_read(self, addr):
         if addr < len(self.ram):
@@ -47,7 +88,32 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        # elif op == "SUB": etc
+        elif op == "AND":
+            raise Exception("Instruction not yet implemented: AND")
+        elif op == "CMP":
+            raise Exception("Instruction not yet implemented: CMP")
+        elif op == "DEC":
+            raise Exception("Instruction not yet implemented: DEC")
+        elif op == "DIV":
+            raise Exception("Instruction not yet implemented: DIV")
+        elif op == "INC":
+            raise Exception("Instruction not yet implemented: INC")
+        elif op == "MOD":
+            raise Exception("Instruction not yet implemented: MOD")
+        elif op == "MUL":
+            raise Exception("Instruction not yet implemented: MUL")
+        elif op == "NOT":
+            raise Exception("Instruction not yet implemented: NOT")
+        elif op == "OR":
+            raise Exception("Instruction not yet implemented: OR")
+        elif op == "SHL":
+            raise Exception("Instruction not yet implemented: SHL")
+        elif op == "SHR":
+            raise Exception("Instruction not yet implemented: SHR")
+        elif op == "SUB":
+            raise Exception("Instruction not yet implemented: SUB")
+        elif op == "XOR":
+            raise Exception("Instruction not yet implemented: XOR")
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -73,4 +139,103 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        running = True
+        while running:
+            # read the byte at the program counter into the Instruction Register
+            ir = self.ram_read(self.pc)
+            # read the next two bytes in case the instruction
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)  # ...needs to utilize them
+
+            if ir not in instr:
+                raise Exception(f"Invlaid instruction {ir}. Terminating.")
+
+            # Direct the CPU to follow the correct instruction
+            if ir == 1:
+                # HLT "halt" instruction, ends program
+                exit()
+            elif ir & 0b00100000 == 32:
+                # this is an ALU instruction - transfer to ALU
+                self.alu(instr[ir], operand_a, operand_b)
+            elif ir & 0b11000000 == 0:
+                # single byte instructions (no operands)
+                getattr(self, instr[ir])()
+            elif ir & 0b11000000 == 64:
+                # two-bye (one operand) instructions
+                getattr(self, instr[ir])(operand_a)
+            elif ir & 0b11000000 == 128:
+                # three-byte (two operand) instructions
+                getattr(self, instr[ir])(operand_a, operand_b)
+
+            # Move the Program Counter
+            if ir & 0b00010000 == 16:
+                # this instruction set the PC, so we won't move it.
+                continue
+            elif ir & 0b11000000 == 0:
+                # single byte instructions (no operands)
+                self.pc += 1
+            elif ir & 0b11000000 == 64:
+                # two-bye (one operand) instructions
+                self.pc += 2
+            elif ir & 0b11000000 == 128:
+                # three-byte (two operand) instructions
+                self.pc += 3
+
+    # Implementation of non-ALU instructions
+    def CALL(self):
+        raise Exception("Instruction not yet implemented: CALL")
+
+    def INT(self):
+        raise Exception("Instruction not yet implemented: INT")
+
+    def IRET(self):
+        raise Exception("Instruction not yet implemented: IRET")
+
+    def JEQ(self):
+        raise Exception("Instruction not yet implemented: JEQ")
+
+    def JGE(self):
+        raise Exception("Instruction not yet implemented: JGE")
+
+    def JGT(self):
+        raise Exception("Instruction not yet implemented: JGT")
+
+    def JLE(self):
+        raise Exception("Instruction not yet implemented: JLE")
+
+    def JLT(self):
+        raise Exception("Instruction not yet implemented: JLT")
+
+    def JMP(self):
+        raise Exception("Instruction not yet implemented: JMP")
+
+    def JNE(self):
+        raise Exception("Instruction not yet implemented: JNE")
+
+    def LD(self):
+        raise Exception("Instruction not yet implemented: LD")
+
+    def LDI(self):
+        raise Exception("Instruction not yet implemented: LDI")
+
+    def NOP(self):
+        raise Exception("Instruction not yet implemented: NOP")
+
+    def POP(self):
+        raise Exception("Instruction not yet implemented: POP")
+
+    def PRA(self):
+        raise Exception("Instruction not yet implemented: PRA")
+
+    def PRN(self):
+        raise Exception("Instruction not yet implemented: PRN")
+
+    def PUSH(self):
+        raise Exception("Instruction not yet implemented: PUSH")
+
+    def RET(self):
+        raise Exception("Instruction not yet implemented: RET")
+
+    def ST(self):
+        raise Exception("Instruction not yet implemented: ST")
